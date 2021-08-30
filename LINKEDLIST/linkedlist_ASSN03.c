c781f081f9c4235f2c19f298f0a09a08c22927fe
Randomystick <bryankhleow@gmail.com>
2021-08-30 17:06:55 +0800

    linked list

---
+++ LINKEDLIST/linkedlist_ASSN03.c
@@ -0,0 +1,262 @@
+//////////////////////////////////////////////////////////////////////////////////
+
+/* CE1107/CZ1107 Data Structures
+Assignment 1 - Linked List Questions
+Purpose: Implementing the required functions for Question 3 */
+
+//////////////////////////////////////////////////////////////////////////////////
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+//////////////////////////////////////////////////////////////////////////////////
+
+typedef struct _listnode
+{
+	int item;
+	struct _listnode *next;
+} ListNode;			// You should not change the definition of ListNode
+
+typedef struct _linkedlist
+{
+	int size;
+	ListNode *head;
+} LinkedList;			// You should not change the definition of LinkedList
+
+
+//////////////////////// function prototypes /////////////////////////////////////
+
+// You should not change the prototype of this function
+void moveOddItemsToBack(LinkedList *ll);
+
+void printList(LinkedList *ll);
+void removeAllItems(LinkedList *ll);
+ListNode * findNode(LinkedList *ll, int index);
+int insertNode(LinkedList *ll, int index, int value);
+int removeNode(LinkedList *ll, int index);
+
+//////////////////////////// main() //////////////////////////////////////////////
+
+int main()
+{
+	LinkedList ll;
+	int c, i, j;
+	c = 1;
+	//Initialize the linked list 1 as an empty linked list
+	ll.head = NULL;
+	ll.size = 0;
+
+
+	printf("1: Insert an integer to the linked list:\n");
+	printf("2: Move all odd integers to the back of the linked list:\n");
+	printf("0: Quit:\n");
+
+	while (c != 0)
+	{
+		printf("Please input your choice(1/2/0): ");
+		scanf("%d", &c);
+
+		switch (c)
+		{
+		case 1:
+			printf("Input an integer that you want to add to the linked list: ");
+			scanf("%d", &i);
+			j = insertNode(&ll, ll.size, i);
+			printf("The resulting linked list is: ");
+			printList(&ll);
+			break;
+		case 2:
+			moveOddItemsToBack(&ll); // You need to code this function
+			printf("The resulting linked list after moving odd integers to the back of the linked list is: ");
+			printList(&ll);
+			removeAllItems(&ll);
+			break;
+		case 0:
+			removeAllItems(&ll);
+			break;
+		default:
+			printf("Choice unknown;\n");
+			break;
+		}
+	}
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+void moveOddItemsToBack(LinkedList *ll)
+{
+//////////////////////////////
+//////////// TODO ////////////
+//////////////////////////////
+// 1 1 2 3 4 5
+// remove index 0
+// 1 2 3 4 5
+// remove index 0
+// 2 3 4 5
+// skip index 0
+// 2 3 4 5
+// remove index 1
+// 2 4 5
+// skip index 2
+// remove index 3
+
+    // get linked list of the odd numbers, and remove them from the original
+    LinkedList oddNodes;
+    oddNodes.head = NULL;
+    oddNodes.size = 0;
+    int oddNodesINDEX = 0;
+
+    int loop = ll->size;
+    int i = 0;
+    while (loop--)
+    {
+        int itemm = findNode(ll, i)->item;
+        if (itemm % 2 != 0) //if current item is odd
+        {
+            insertNode(&oddNodes, oddNodesINDEX, itemm);
+            oddNodesINDEX++;
+            removeNode(ll, i);
+        }
+        else
+        {
+            i++;
+        }
+    }
+    //printList(ll);
+    //printf("\n\n");
+    //printList(&oddNodes);
+
+    // now, take the odd numbers linked list and append back to the original
+    while (oddNodes.head != NULL) //oddNodes is a localhosted linked list, so just use .dot
+    {
+        insertNode(ll, ll->size, oddNodes.head->item); //however, the oddNodes.head is a pointer to a listnode, so oddNodes.head still needs -> to reference item
+        removeNode(&oddNodes, 0);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
+void printList(LinkedList *ll){
+
+	ListNode *cur;
+	if (ll == NULL)
+		return;
+	cur = ll->head;
+
+	if (cur == NULL)
+		printf("Empty");
+	while (cur != NULL)
+	{
+		printf("%d ", cur->item);
+		cur = cur->next;
+	}
+	printf("\n");
+}
+
+
+void removeAllItems(LinkedList *ll)
+{
+	ListNode *cur = ll->head;
+	ListNode *tmp;
+
+	while (cur != NULL){
+		tmp = cur->next;
+		free(cur);
+		cur = tmp;
+	}
+	ll->head = NULL;
+	ll->size = 0;
+}
+
+
+ListNode *findNode(LinkedList *ll, int index){
+
+	ListNode *temp;
+
+	if (ll == NULL || index < 0 || index >= ll->size)
+		return NULL;
+
+	temp = ll->head;
+
+	if (temp == NULL || index < 0)
+		return NULL;
+
+	while (index > 0){
+		temp = temp->next;
+		if (temp == NULL)
+			return NULL;
+		index--;
+	}
+
+	return temp;
+}
+
+int insertNode(LinkedList *ll, int index, int value){
+
+	ListNode *pre, *cur;
+
+	if (ll == NULL || index < 0 || index > ll->size + 1)
+		return -1;
+
+	// If empty list or inserting first node, need to update head pointer
+	if (ll->head == NULL || index == 0){
+		cur = ll->head;
+		ll->head = malloc(sizeof(ListNode));
+		ll->head->item = value;
+		ll->head->next = cur;
+		ll->size++;
+		return 0;
+	}
+
+
+	// Find the nodes before and at the target position
+	// Create a new node and reconnect the links
+	if ((pre = findNode(ll, index - 1)) != NULL){
+		cur = pre->next;
+		pre->next = malloc(sizeof(ListNode));
+		pre->next->item = value;
+		pre->next->next = cur;
+		ll->size++;
+		return 0;
+	}
+
+	return -1;
+}
+
+
+int removeNode(LinkedList *ll, int index){
+
+	ListNode *pre, *cur;
+
+	// Highest index we can remove is size-1
+	if (ll == NULL || index < 0 || index >= ll->size)
+		return -1;
+
+	// If removing first node, need to update head pointer
+	if (index == 0){
+		cur = ll->head->next;
+		free(ll->head);
+		ll->head = cur;
+		ll->size--;
+
+		return 0;
+	}
+
+	// Find the nodes before and after the target position
+	// Free the target node and reconnect the links
+	if ((pre = findNode(ll, index - 1)) != NULL){
+
+		if (pre->next == NULL)
+			return -1;
+
+		cur = pre->next;
+		pre->next = cur->next;
+		free(cur);
+		ll->size--;
+		return 0;
+	}
+
+	return -1;
+}
+//*/
---
+++ LINKEDLIST/test.c
@@ -0,0 +1,10 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int lol = 5/2;
+    printf("%d", lol);
+}
+//*/
